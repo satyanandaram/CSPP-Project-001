b00700971ef54f749a76d791ebabe01178821a2f
LaxmiNarayanaMurthyVemuri <murthyvemuri@msitprogram.net>
2021-09-18 15:47:40 +0530

    changes made to wc

--- wc.py
+++ wc.py
@@ -2,7 +2,53 @@
 
 import sys
 
-from lib.helper import wc, readfile
+def wc(text):
+    """
+        will create a function of wc which takes text(i.e., content of the entire file)
+        as a parameter
+    """
+    word_count = word_counts(text)
+    char_count = char_counts(text)
+    line_count = line_counts(text)
+    byte_count = byte_counts(text)
+    return [line_count, word_count, byte_count, char_count]
+
+def word_counts(text):
+    """
+        returns the count of words a given file
+    """
+    list_of_words = text.split()
+    return len(list_of_words)
+
+def line_counts(text):
+    """
+        returns the count of lines a given file
+    """
+    list_of_lines = text.split("\n")
+    return len(list_of_lines) - 1
+
+def char_counts(text):
+    """
+        returns the count of character a given file
+    """
+    return len(text)
+
+def byte_counts(text):
+    """
+        returns the count of bytes a given file
+    """
+    return len(text.encode('utf-8'))
+
+def readfile(filename):
+    """
+        Reading the contents of a given file
+    """
+    try:
+        file = open(filename)
+        text = file.read()
+        return text
+    except FileNotFoundError:
+        print("tail: cannot open", filename, "for reading: No such file or directory")
 
 
 if __name__ == "__main__":
